09533ff7746e149083b4dde111a308fe3971f0b5
Matheus Kuster <matheuskuster@hotmail.com>
2019-04-13 22:17:06 -0300

    Cors

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
-
+const cors = require('cors');
 
 mongoose.connect(`mongodb+srv://sigi:${process.env.MONGO_PASSWORD}@cluster0-cdj1t.mongodb.net/inscricao_sigi?retryWrites=true`, {
     useNewUrlParser: true
 })
 
-
 const app = express()
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
