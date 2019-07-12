59031b092a6904f3b0eb92702c3548cd5f53ae48
Matheus Kuster <matheuskuster@hotmail.com>
2019-07-12 15:27:11 -0300

    Block subscribe routes

--- src/routes.js
+++ src/routes.js
@@ -6,15 +6,15 @@ const multerConfig = require("./config/multer");
 
 router.get("/show/:cnpj", SubscriptionController.showSchool);
 
-router.post(
-  "/term/:id",
-  multer(multerConfig).single("term"),
-  SubscriptionController.uploadTerm
-);
+// router.post(
+//   "/term/:id",
+//   multer(multerConfig).single("term"),
+//   SubscriptionController.uploadTerm
+// );
 
-router.post("/subscription", SubscriptionController.handleSubscription);
+// router.post("/subscription", SubscriptionController.handleSubscription);
 
-router.get("/sheet/:cnpj", SubscriptionController.createSheet);
+// router.get("/sheet/:cnpj", SubscriptionController.createSheet);
 
 router.get("/show", SubscriptionController.show);
 
